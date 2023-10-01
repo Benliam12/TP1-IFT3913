fd84668e5f761c1c436d34d3000bea6470402184
Benliam12 <s100live.ca@live.ca>
2023-10-01 19:28:31 -0400

    Adding a jar file for Tloc

---
+++ jarfiles/TestFileTLoc.java
@@ -0,0 +1,35 @@
+public class TestFile {
+    /*
+    /*
+    /*
+    /* lksadjlkdsajsldkaj
+    /*
+    /*
+    /*
+    /*
+    This should be a 2 line program.
+     */
+    /**
+*
+     * /
+     * /
+     /*
+     /////*
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     */
+    public void haha(){
+        //
+        System.out.println("POGGIES");
+        /*
+        Another commented line
+         */
+        System.out.println("SOME MORE CODE");
+    }
+}
