52114b0ff9f8a9936eed6ccd6d6ba86d733bce55
Benliam12 <s100live.ca@live.ca>
2023-10-01 20:58:20 -0400

    Updating only TLoc TestFile

--- tloc/src/TestFile.java
+++ tloc/src/TestFile.java
@@ -1,3 +1,7 @@
+/**
+ * Test class
+ * This file, when pass into the TLoc program, should return 6.
+ */
 public class TestFile {
     /*
     /*
@@ -7,15 +11,24 @@ public class TestFile {
     /*
     /*
     /*
-    This should be a 2 line program.
+
      */
     /**
-*
+     *
      * /
      * /
      /*
      /////*
      *
+     *
+     *
+     *
+     *
+
+
+
+
+
      *
      *
      *
@@ -29,7 +42,39 @@ public class TestFile {
         System.out.println("POGGIES");
         /*
         Another commented line
+
+
+
+
+
+
+
+
+
+
          */
+
+
+
+        /* // Something amazing IG */
+        /**/
+
+        /* Something kinda weird */
+
+
+
+
         System.out.println("SOME MORE CODE");
     }
+
+
+
+
+
+
+
+
+
+
+
 }
