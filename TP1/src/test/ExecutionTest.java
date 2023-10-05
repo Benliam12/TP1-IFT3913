af511f0751d63ae008c14bc8438bffab13063991
Benliam12 <s100live.ca@live.ca>
2023-10-05 18:22:55 -0400

    Updating execution tests

--- TP1/src/test/ExecutionTest.java
+++ TP1/src/test/ExecutionTest.java
@@ -13,98 +13,95 @@ import java.util.ArrayList;
 public class ExecutionTest {
     @Test
     public void ExecuteTloc() throws IOException {
-        Process process = Runtime.getRuntime().exec("java -jar ../jarfiles/tloc.jar ../../jfreechart-master/src/test/java/org/jfree/chart/title/TitleTest.java");
-        InputStream inputStream = process.getInputStream();
-        // Create a BufferedReader to read lines from the input stream
-        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-        String line;
-        line = reader.readLine();
-
-        Assert.assertNotNull(line);
+        ArrayList<String> data = executeJar("java -jar ../jarfiles/tloc.jar ../../jfreechart-master/src/test/java/org/jfree/chart/title/TitleTest.java");
+        Assert.assertFalse(data.isEmpty());
         try{
-            Assert.assertEquals(39,Integer.parseInt(line));
+            Assert.assertEquals(39,Integer.parseInt(data.get(0)));
         } catch(NumberFormatException e){
             Assert.fail();
         }
+    }
+
+    @Test
+    public void ExecuteTlocInvalidPath() throws IOException {
+        ArrayList<String> data = executeJar("java -jar ../jarfiles/tloc.jar e");
+        Assert.assertFalse(data.isEmpty());
+        Assert.assertEquals("File or directory does not exists!", data.get(0));
+    }
+
+    @Test
+    public void ExecuteTlocNoPath() throws IOException {
+        ArrayList<String> data = executeJar("java -jar ../jarfiles/tloc.jar");
+        Assert.assertFalse(data.isEmpty());
+        Assert.assertEquals("Please provide a path!", data.get(0));
     }
 
     @Test
     public void ExecuteTAssert() throws IOException {
-        Process process = Runtime.getRuntime().exec("java -jar ../jarfiles/tassert.jar ../../jfreechart-master/src/test/java/org/jfree/chart/title/TitleTest.java");
-        InputStream inputStream = process.getInputStream();
-        // Create a BufferedReader to read lines from the input stream
-        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-        String line;
-        line = reader.readLine();
-
-        Assert.assertNotNull(line);
+        ArrayList<String> data = executeJar("java -jar ../jarfiles/tassert.jar ../../jfreechart-master/src/test/java/org/jfree/chart/title/TitleTest.java");
+        Assert.assertFalse(data.isEmpty());
         try{
-            Assert.assertEquals(11,Integer.parseInt(line));
+            Assert.assertEquals(11,Integer.parseInt(data.get(0)));
         } catch(NumberFormatException e){
             Assert.fail();
         }
     }
+    @Test
+    public void ExecuteTassertInvalidPath() throws IOException {
+        ArrayList<String> data = executeJar("java -jar ../jarfiles/tassert.jar e");
+        Assert.assertFalse(data.isEmpty());
+        Assert.assertEquals("File or directory does not exists!", data.get(0));
+    }
+    @Test
+    public void ExecuteTAssertNoPath() throws IOException {
+        ArrayList<String> data = executeJar("java -jar ../jarfiles/tassert.jar");
+        Assert.assertFalse(data.isEmpty());
+        Assert.assertEquals("Please provide a path!", data.get(0));
+    }
 
     @Test
     public void ExecuteTls() throws IOException {
-        Process process = Runtime.getRuntime().exec("java -jar ../jarfiles/tls.jar ../../jfreechart-master/src/test/");
-        InputStream inputStream = process.getInputStream();
-        // Create a BufferedReader to read lines from the input stream
-        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-        String line;
-        ArrayList<String> lineData = new ArrayList<>();
-        while ((line = reader.readLine()) != null) {
-            lineData.add(line);
-        }
-
+        ArrayList<String> lineData = executeJar("java -jar ../jarfiles/tls.jar ../../jfreechart-master/src/test/");
         Assert.assertEquals(350, lineData.size());
 
     }
 
     @Test
     public void ExecuteTlsFileOut() throws IOException {
-        Process process = Runtime.getRuntime().exec("java -jar ../jarfiles/tls.jar -o output.csv ../../jfreechart-master/src/test/");
-        InputStream inputStream = process.getInputStream();
-        // Create a BufferedReader to read lines from the input stream
-        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-
-        String line;
-        while ((line = reader.readLine()) != null) {
-        }
+        executeJar("java -jar ../jarfiles/tls.jar -o output.csv ../../jfreechart-master/src/test/");
         File f = new File("output.csv");
-
         Assert.assertTrue(f.exists());
         Assert.assertTrue(Files.size(Path.of(f.getAbsolutePath())) > 0);
     }
 
     @Test
     public void ExecuteTropcomp() throws IOException {
-        Process process = Runtime.getRuntime().exec("java -jar ../jarfiles/tropcomp.jar ../../jfreechart-master/src/test/ 20");
-        InputStream inputStream = process.getInputStream();
-        // Create a BufferedReader to read lines from the input stream
-        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-
-        String line;
-        ArrayList<String> s = new ArrayList<>();
-        while ((line = reader.readLine()) != null) {
-            s.add(line);
-        }
+        ArrayList<String> s = executeJar("java -jar ../jarfiles/tropcomp.jar ../../jfreechart-master/src/test/ 20");
         Assert.assertTrue(s.size() > 1);
     }
     @Test
     public void ExecuteTropcompFileOut() throws IOException {
-        Process process = Runtime.getRuntime().exec("java -jar ../jarfiles/tropcomp.jar -o output.csv ../../jfreechart-master/src/test/ 20");
-        InputStream inputStream = process.getInputStream();
-        // Create a BufferedReader to read lines from the input stream
-        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-
-        String line;
-        while ((line = reader.readLine()) != null) {
-        }
+        executeJar("java -jar ../jarfiles/tropcomp.jar -o output.csv ../../jfreechart-master/src/test/ 20");
         File f = new File("output.csv");
-
         Assert.assertTrue(f.exists());
         Assert.assertTrue(Files.size(Path.of(f.getAbsolutePath())) > 0);
+    }
+
+    private ArrayList<String> executeJar(String command){
+        ArrayList<String> output = new ArrayList<>();
+        try{
+            Process process = Runtime.getRuntime().exec(command);
+            InputStream inputStream = process.getInputStream();
+            // Create a BufferedReader to read lines from the input stream
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                output.add(line.trim());
+            }
+        } catch (IOException e){
+        }
+        return output;
     }
 
     @After
